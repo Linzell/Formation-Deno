9e22614ca157f06eb1150984d87c8f4f647da49c
Charlie Cohen <linzellart@gmail.com>
2022-05-24 11:09:38 +0200

    Update

---
+++ jx_basic/main.jsx
@@ -0,0 +1,30 @@
+import { parseHTML } from "https://esm.sh/linkedom@0.14.9";
+
+const { document, customElements, HTMLElement } = parseHTML(`<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <title>Hello from Deno</title>
+    </head>
+    <body>
+      <h1>Hello from Deno</h1>
+      <form>
+        <input name="user">
+        <button>
+          Submit
+        </button>
+      </form>
+    </body>
+  </html>`);
+
+customElements.define(
+  "custom-element",
+  class extends HTMLElement {
+    connectedCallback() {
+      console.log("it works 🥳");
+    }
+  },
+);
+
+document.body.appendChild(document.createElement("custom-element"));
+
+document.toString(); // the string of the document, ready to send to a client
